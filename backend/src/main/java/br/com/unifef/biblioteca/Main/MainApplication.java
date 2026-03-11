d218f308e8b611f896a596a179fe4c2c3ac4ead2
lmateusfaria <luis.faria@fatec.sp.gov.br>
2026-03-11 14:51:43 -0300

    fix: habilitando scan de repositórios e entidades do Neo4j no MainApplication

--- backend/src/main/java/br/com/unifef/biblioteca/Main/MainApplication.java
+++ backend/src/main/java/br/com/unifef/biblioteca/Main/MainApplication.java
@@ -4,13 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @ComponentScan(basePackages = "br.com.unifef.biblioteca")
-@EntityScan(basePackages = {"br.com.unifef.biblioteca.domains","br.com.unifef.biblioteca.domains.enums"})
+@EntityScan(basePackages = {
+    "br.com.unifef.biblioteca.domains",
+    "br.com.unifef.biblioteca.domains.enums",
+    "br.com.unifef.biblioteca.domains.graph"
+})
 @EnableJpaRepositories(basePackages = "br.com.unifef.biblioteca.repositories")
+@EnableNeo4jRepositories(basePackages = "br.com.unifef.biblioteca.repositories.graph")
 @EnableScheduling
 @SpringBootApplication
 public class MainApplication {
